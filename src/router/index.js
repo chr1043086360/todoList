94e8c3ee971c49ca83e2c223c24c7f88bede41af
“chr” <1043086360@qq.com>
2020-02-28 19:54:21 +0800

    .

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,20 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Index from '../views/Index.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Index from "../views/Index.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'index',
-    component: Index,
-  }
-]
+    path: "/",
+    name: "index",
+    component: Index
+  } //去除#号
+];
 
 const router = new VueRouter({
-  routes
-})
+  routes,
+  mode: "history" //去除#号
+});
 
-export default router
+export default router;
